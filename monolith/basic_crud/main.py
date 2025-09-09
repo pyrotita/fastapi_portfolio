9976aeca1b0110ba6e39c436533bc7ccb2b3b355
poi <jesuszzvaleta@gmail.com>
2025-09-09 04:25:15 -0600

    add utilities

--- monolith/basic_crud/main.py
+++ monolith/basic_crud/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 
+#¿?
+from database import Tasks
 
+
+#<·
 app: FastAPI = FastAPI(
     title='Basic crud impl',
     debug=False,
 )
 
 @app.get('/')
-def root() -> dict:
+def root(iden: str) -> dict:
     return {}
